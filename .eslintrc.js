2499b4254eaddee5e4991846bea4f554f5819173
c.murakami <c.murakami@officekoma.co.jp>
2021-01-11 18:20:15 +0900

    added eslint and prettier (Codes are formatted automatically when you commit)

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+	globals: {
+		document: true,
+	},
+	env: {
+		commonjs: true,
+		es2021: true,
+		node: true,
+	},
+	extends: 'airbnb-base',
+	rules: {
+		'no-tabs': 0,
+		indent: [2, 'tab'],
+		allowIndentationTabs: 0,
+		semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+		'semi-spacing': ['error', { after: true, before: false }],
+		'semi-style': ['error', 'first'],
+		'no-extra-semi': 'error',
+		'no-unexpected-multiline': 'error',
+		'no-unreachable': 'error',
+	},
+}
